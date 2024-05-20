8dde2f59df665eb42291bec27690840ae4faf266
Perna <joaogilperna@gmail.com>
2024-05-20 14:09:25 -0300

    Correção dash e exclusão de arquivos

--- Curso 10/Modulo 3 Dash.py
+++ Curso 10/Modulo 3 Dash.py
@@ -9,7 +9,7 @@ import plotly.express as px
 from dash import no_update
 
 # Read the airline data into pandas dataframe
-spacex_df = pd.read_csv("spacex_launch_dash.csv")
+spacex_df = pd.read_csv("spacex_launch_geo.csv")
 max_payload = spacex_df['Payload Mass (kg)'].max()
 min_payload = spacex_df['Payload Mass (kg)'].min()
 
